96e1d1866b0e6696c9cc6ceddccc414d5d7b57a9
Xiwen Teoh <teoh6g@gmail.com>
2022-05-10 22:51:19 +0800

    Added wallet connect/disconnect function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
 import './App.css';
+import { useWallet, WalletStatus } from "@terra-money/wallet-provider";
+import Menu from './components/Menu';
 
 function App() {
+  const { status, connect, disconnect, availableConnectTypes } = useWallet();
+
+  const renderConnectButton = () => {
+    if (status === WalletStatus.WALLET_NOT_CONNECTED) {
+      return (
+        <div className="connect-wallet-div">
+          <button
+            type="button"
+            key={`connect-EXTENSION`}
+            onClick={() => connect("EXTENSION")}
+            className="cta-button connect-wallet-button"
+          >
+            Connect wallet
+          </button>
+        </div>
+      );
+    }
+    else if (status === WalletStatus.WALLET_CONNECTED) {
+      return (
+        <button
+          type="button"
+          onClick={() => disconnect()}
+          className="cta-button connect-wallet-button"
+        >
+          Disconnect
+        </button>
+      );
+    }
+  };
+  
+  // Let's take a look at what the starting states are!
+  console.log("Wallet status is ", status);
+  console.log("Available connection types:", availableConnectTypes);
 
   return (
     <main className="App">
@@ -9,15 +44,27 @@ function App() {
           <h1>⚔ Goblin War ⚔</h1>
           <p>Only you can save us from Goblin town</p>
         </div>
-
       </header>
 
-      <div>
-        <img
-          src="https://media.giphy.com/media/B19AYwNXoXtcs/giphy.gif"
-          alt="Goblin gif"
-        />
-      </div>
+      {/* If not connected, show the goblin GIF! */}
+      {status === WalletStatus.WALLET_NOT_CONNECTED && (
+        <div>
+          <img
+            src="https://media.giphy.com/media/B19AYwNXoXtcs/giphy.gif"
+            alt="Goblin gif"
+          />
+        </div>
+      )}
+
+      {/* Show the menu after connection */}
+      {status === WalletStatus.WALLET_CONNECTED && (
+          <div className="game-menu-container">
+            <Menu />
+          </div>
+      )}
+
+      {renderConnectButton()}
+
     </main>
   );
 }
